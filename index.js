30bc4929770fc099ba7806c6a2dee7e1cc6d53be
Eric Schwartz <eric@kenzie.academy>
2019-05-08 22:54:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const fastcsv = require('fast-csv');
+const fs = require('fs');
+const path = require('path');
+
+//array to store rows as they are read from the csv
+let records = [];
+
+//make sure we have a filename passed in from the command line
+if (process.argv.length <= 2) {
+    console.log("Usage: node " + path.basename(__filename) + " CSV_FILE");
+    process.exit(-1);
+}
+
+//set the filename from the command line
+const file = process.argv[2];
+
+//make sure file exists
+if (!fs.existsSync(file)) {
+    console.log(`Aborted: ${file} does not exist`);
+    process.exit(-1);
+  }
+
+console.log('Started: Reading CSV Data');
+fastcsv.fromPath(file, { headers: false, ignoreEmpty: false })
+    .on("data", data => {
+        
+        //we don't want to continue if the file has more than 2 columns
+        if (data.length > 2) {
+            console.log(`Aborted: ${file} has more than two columns of data`);
+            process.exit(-1);
+        }
+
+        //if we have 2 columns then we need to see if the second is blank.
+        //sometimes the file has the comman but no data for the second column
+        if(data.length === 2){
+            //if we have valid data in the second column, abort
+            if(data[1] !== ""){
+                console.log(`Aborted: ${file} already has 2 valid colums of data`);
+                process.exit(-1);
+            }
+
+            //remove the empty column and continue.
+            data.length = 1;
+        }
+
+        //create a new record in our records with the duplicated column
+        records.push([data[0], data[0]])
+    })
+    .on("end", () => {
+        console.log('Completed: Reading CSV Data');
+        //write the new records to their own csv
+        write_file(records)
+    });
+
+const write_file = (data) => {
+    console.log('Started: Writing CSV Data');
+    const ws = fs.createWriteStream(`updated_${file}`);
+    fastcsv
+        .write(data, { headers: false })
+        .pipe(ws)
+        .on("finish", function () {
+            console.log('Completed: Writing CSV Data');
+        });
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# CSV Column Duplicator 
+This application takes a csv file with one column and outputs a new csv with 2 columns. The second column is a duplicate of the first column. It was created to because the CSV files being parsed have over a million rows in them. Doing this with standard tools was taking ~30-60 minutes. This script is to serve as a proof of concept. It accomplishes the task in a ~10 seconds for a file with over 1 million rows.
+
+## Installation
+
+* Clone this repo
+* cd into to repo and run `npm -i`
+
+## Usage
+You have been provided two sample csv files. **data_hundred.csv** contains 100 rows of data. **data_million.csv** contains over 1,000,000 rows of data.
+
+**Example Usage:**
+
+```bash
+$ node index CSV_FILE
+```
+
+**Proccess sample data:**
+
+Outputs updates_data_million.csv with added column
+```bash
+$ node index data_million.csv
+```
