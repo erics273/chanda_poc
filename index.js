792fcb4c57a05419eba60f1f72897c1f7d017098
Eric Schwartz <eric@kenzie.academy>
2019-05-09 00:44:01 -0400

    refactor because im bored and wanted to play with stuff

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 //make sure we have a filename passed in from the command line
 if (process.argv.length <= 2) {
-    console.log("Usage: node " + path.basename(__filename) + " CSV_FILE");
+    console.error("Usage: node " + path.basename(__filename) + " CSV_FILE");
     process.exit(-1);
 }
 
@@ -13,55 +13,61 @@ const file = process.argv[2];
 
 //make sure file exists
 if (!fs.existsSync(file)) {
-    console.log(`Aborted: ${file} does not exist`);
+    console.error(`Aborted: ${file} does not exist`);
     process.exit(-1);
 }
 
 //define method to process the csv
 //filePath (String): path to csv file
 //callback (Function): Receives Array of csv rows as its parameter 
-const processCsv = (filePath, callback) => {
+const readCSV = (filePath) => {
 
-    console.log('Started: Reading CSV Data');
+    return new Promise(function (resolve, reject) {
 
-    //array to store rows as they are read from the csv
-    let records = [];
+        console.log('Started: Reading CSV Data');
 
-    fastcsv.fromPath(filePath, { headers: false, ignoreEmpty: false })
-        .on("data", data => {
+        //array to store rows as they are read from the csv
+        let records = [];
 
-            //we don't want to continue if the file has more than 2 columns
-            if (data.length > 2) {
-                console.log(`Aborted: ${filePath} has more than two columns of data`);
-                process.exit(-1);
-            }
+        fastcsv.fromPath(filePath, { headers: false, ignoreEmpty: false })
+            .on("data", data => {
 
-            //if we have 2 columns then we need to see if the second is blank.
-            //sometimes the file has the comman but no data for the second column
-            if (data.length === 2) {
-                //if we have valid data in the second column, abort
-                if (data[1] !== "") {
-                    console.log(`Aborted: ${filePath} already has 2 valid colums of data`);
+                //we don't want to continue if the file has more than 2 columns
+                if (data.length > 2) {
+                    console.error(`Aborted: ${filePath} has more than two columns of data`);
                     process.exit(-1);
                 }
 
-                //remove the empty column and continue.
-                data.length = 1;
-            }
+                //if we have 2 columns then we need to see if the second is blank.
+                //sometimes the file has the comman but no data for the second column
+                if (data.length === 2) {
+                    //if we have valid data in the second column, abort
+                    if (data[1] !== "") {
+                        console.error(`Aborted: ${filePath} already has 2 valid colums of data`);
+                        process.exit(-1);
+                    }
 
-            //create a new record in our records with the duplicated column
-            records.push([data[0], data[0]])
-        })
-        .on("end", () => {
-            console.log(`Completed: Reading CSV Data (${records.length.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} rows)`);
-            //write the new records to their own csv
-            callback(records)
-        });
+                    //remove the empty column and continue.
+                    data.length = 1;
+                }
+
+                //create a new record in our records with the duplicated column
+                records.push([data[0], data[0]])
+            })
+            .on("error", () => {
+                //write the new records to their own csv
+                reject(`Aborted: ${filePath} failed to process for unknown reason`)
+            })
+            .on("end", () => {
+                console.log(`Completed: Reading CSV Data (${records.length.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} rows)`);
+                //write the new records to their own csv
+                resolve(records)
+            });
+
+    })
 }
 
-//process the csv file providing a callback function 
-//that writes the records to a new csv file
-processCsv(file, (data) => {
+const writeCSV = (data) => {
     console.log('Started: Writing CSV Data');
     const ws = fs.createWriteStream(`updated_${file}`);
     fastcsv
@@ -70,4 +76,7 @@ processCsv(file, (data) => {
         .on("finish", function () {
             console.log('Completed: Writing CSV Data');
         });
-})
+}
+
+//read in the csv file and when finished write the records to a new csv
+readCSV(file).then(writeCSV)
