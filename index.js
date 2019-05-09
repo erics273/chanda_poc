92cd3c8df33a92dfa04dc72cdb8b07a351ba92bf
Eric Schwartz <eric@kenzie.academy>
2019-05-09 01:48:44 -0400

    refactor because im bored and wanted to play with stuff

--- index.js
+++ index.js
@@ -8,23 +8,12 @@ if (process.argv.length <= 2) {
     process.exit(-1);
 }
 
-//set the filename from the command line
-const file = process.argv[2];
-
-//make sure file exists
-if (!fs.existsSync(file)) {
-    console.error(`Aborted: ${file} does not exist`);
-    process.exit(-1);
-}
-
 //define method to process the csv
 //filePath (String): path to csv file
-//callback (Function): Receives Array of csv rows as its parameter 
+//returns a promise that is resolved when all rows are processed
 const readCSV = (filePath) => {
 
     return new Promise(function (resolve, reject) {
-
-        console.log('Started: Reading CSV Data');
 
         //array to store rows as they are read from the csv
         let records = [];
@@ -34,8 +23,7 @@ const readCSV = (filePath) => {
 
                 //we don't want to continue if the file has more than 2 columns
                 if (data.length > 2) {
-                    console.error(`Aborted: ${filePath} has more than two columns of data`);
-                    process.exit(-1);
+                    throw (`${filePath} has more than two columns of data`);
                 }
 
                 //if we have 2 columns then we need to see if the second is blank.
@@ -43,8 +31,7 @@ const readCSV = (filePath) => {
                 if (data.length === 2) {
                     //if we have valid data in the second column, abort
                     if (data[1] !== "") {
-                        console.error(`Aborted: ${filePath} already has 2 valid colums of data`);
-                        process.exit(-1);
+                        throw (`${filePath} already has 2 valid colums of data`);
                     }
 
                     //remove the empty column and continue.
@@ -54,12 +41,11 @@ const readCSV = (filePath) => {
                 //create a new record in our records with the duplicated column
                 records.push([data[0], data[0]])
             })
-            .on("error", () => {
-                //write the new records to their own csv
-                reject(`Aborted: ${filePath} failed to process for unknown reason`)
+            .on("error", (err) => {
+                //reject the promise if linbrary fails to proccess csv
+                reject(err)
             })
             .on("end", () => {
-                console.log(`Completed: Reading CSV Data (${records.length.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} rows)`);
                 //write the new records to their own csv
                 resolve(records)
             });
@@ -67,16 +53,49 @@ const readCSV = (filePath) => {
     })
 }
 
+//create csv
+//returns promise
 const writeCSV = (data) => {
-    console.log('Started: Writing CSV Data');
-    const ws = fs.createWriteStream(`updated_${file}`);
-    fastcsv
-        .write(data, { headers: false })
-        .pipe(ws)
-        .on("finish", function () {
-            console.log('Completed: Writing CSV Data');
-        });
+
+    return new Promise(function (resolve, reject) {
+
+        const ws = fs.createWriteStream(`updated_${file}`);
+        fastcsv
+            .write(data, { headers: false })
+            .pipe(ws)
+            .on("error", function () {
+                reject("Error writing CSV file");
+            })
+            .on("finish", function () {
+                resolve();
+            });
+    })
 }
 
+
+
+//set the filename from the command line
+const file = process.argv[2];
+
+//make sure file exists
+if (!fs.existsSync(file)) {
+    throw (`${file} does not exist`);
+}
 //read in the csv file and when finished write the records to a new csv
-readCSV(file).then(writeCSV)
+console.log('Started: Reading CSV Data');
+readCSV(file)
+    .then((data) => {
+
+        console.log(`Completed: Reading CSV Data (${data.length.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} rows)`);
+        console.log('Started: Writing CSV Data');
+
+        writeCSV(data)
+            .then(() => {
+                console.log('Completed: Writing CSV Data')
+            })
+
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
